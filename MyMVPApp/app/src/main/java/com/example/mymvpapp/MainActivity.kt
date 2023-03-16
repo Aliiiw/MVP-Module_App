e9ad19cdd8462f1190aebcfa421e7ffdbcf8880d
Ali Rahimi <1aliirahimi1@gmail.com>
2023-03-16 12:51:20 +0330

    add composable functions to see the request:

--- MyMVPApp/app/src/main/java/com/example/mymvpapp/MainActivity.kt
+++ MyMVPApp/app/src/main/java/com/example/mymvpapp/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.example.mymvpapp.model.Comment
 import com.example.mymvpapp.model.Post
 import com.example.mymvpapp.presenter.contract.*
 import com.example.mymvpapp.presenter.present.*
 import com.example.mymvpapp.ui.theme.MyMVPAppTheme
+
+lateinit var posts: List<Post>
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,28 +35,58 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 
-                    //Login(username = "username", password = "123")
+                    var postList by remember {
+                        mutableStateOf(emptyList<Post>())
+                    }
 
                     lifecycleScope.launchWhenCreated {
-                        //getAllPostsRequest()
+
+                        postList = getAllPostsRequest()
                         //getPostById(postId = "36")
                         //getAllPostComments(postId = 2)
 
-                        val newPost = Post(
-                            id = 101,
-                            title = "Say hello",
-                            userId = 5,
-                            body = "Hi Aliiiw"
-                        )
+//                        val newPost = Post(
+//                            id = 101,
+//                            title = "Say hello",
+//                            userId = 5,
+//                            body = "Hi Aliiiw"
+//                        )
 
-                        createNewPost(newPost = newPost)
+                        //createNewPost(newPost = newPost)
                     }
+
+                    Column() {
+                        PostView(postList = postList)
+                    }
+
+
+                    //Login(username = "username", password = "123")
+
 
                 }
             }
         }
     }
 }
+
+
+@Composable
+fun PostView(postList: List<Post>) {
+    LazyColumn() {
+        items(postList) { post ->
+            Column(modifier = Modifier
+                .padding(12.dp)
+                .background(Color.Blue)) {
+
+                Text(text = post.title, color = Color.White)
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(text = post.body, color = Color.DarkGray)
+            }
+
+        }
+    }
+}
+
 
 @Composable
 fun Login(username: String, password: String) {
@@ -65,12 +103,13 @@ fun Login(username: String, password: String) {
     presenter.login(username = username, password = password)
 }
 
-suspend fun getAllPostsRequest() {
+suspend fun getAllPostsRequest(): List<Post> {
     val presenter = PostPresenter(object : IPost.View {
         override fun onSuccess(postList: List<Post>) {
-            postList.forEach { post ->
-                Log.e("2323", post.title)
-            }
+            posts = postList
+//            postList.forEach { post ->
+//                Log.e("2323", post.title)
+//            }
         }
 
         override fun onError(message: String) {
@@ -84,6 +123,8 @@ suspend fun getAllPostsRequest() {
     })
 
     presenter.getAllPostsRequest()
+
+    return posts
 
 }
 
